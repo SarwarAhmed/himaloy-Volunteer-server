80266d9a606884955f673debc2bd52a0098a4f39
Sarwar Ahmed <sarwar.amas@gmail.com>
2024-05-16 08:45:14 +0600

    JWT key key generated

--- index.js
+++ index.js
@@ -1,22 +1,44 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const cookieParser = require('cookie-parser');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
-
+const jwt = require('jsonwebtoken')
 const app = express();
+
+const corsOptions = {
+    origin: ['http://localhost:5173'],
+    credentials: true,
+    optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser());
+
+// verify jwt middleware
+const verifyToken = (req, res, next) => {
+    const token = req.cookies?.token
+    if (!token) return res.status(401).send({ message: 'unauthorized access' })
+    if (token) {
+        jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+            if (err) {
+                console.log(err)
+                return res.status(401).send({ message: 'unauthorized access' })
+            }
+            console.log(decoded)
+
+            req.user = decoded
+            next()
+        })
+    }
+}
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5mrfovz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 
-const corsOptions = {
-    origin: ['http://localhost:5173', 'http://localhost:5000'],
-    credentials: true,
-    optionsSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
-
-app.use(express.json());
 
 const client = new MongoClient(uri, {
     serverApi: {
@@ -28,6 +50,41 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
+
+        const VolunteerCollection = client.db('soloSphere').collection('post')
+
+        // jwt generate
+        app.post('/jwt', async (req, res) => {
+            const email = req.body
+            const token = jwt.sign(email, process.env.JWT_SECRET, {
+                expiresIn: '365d',
+            })
+            res
+                .cookie('token', token, {
+                    httpOnly: true,
+                    // secure: process.env.NODE_ENV === 'production',
+                    // sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+                    secure: true,
+                    sameSite: 'None',
+                })
+                .send({ success: true })
+        })
+
+        // Clear token on logout
+        app.get('/logout', (req, res) => {
+            res
+                .clearCookie('token', {
+                    httpOnly: true,
+                    // secure: process.env.NODE_ENV === 'production',
+                    // sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+                    secure: true,
+                    sameSite: 'None',
+                    maxAge: 0,
+                })
+                .send({ success: true })
+        })
+
+
         // Connect the client to the server	(optional starting in v4.7)
         // await client.connect();
         // Send a ping to confirm a successful connection
