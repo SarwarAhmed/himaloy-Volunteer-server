a4e35297f60456d2287084db276611adf9b2d4b9
Sarwar Ahmed <sarwar.amas@gmail.com>
2024-05-16 08:21:19 +0600

    Connect MongoDB

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5mrfovz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
 
 const corsOptions = {
     origin: ['http://localhost:5173', 'http://localhost:5000'],
@@ -13,6 +17,28 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 app.use(express.json());
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Hello from solo!');
