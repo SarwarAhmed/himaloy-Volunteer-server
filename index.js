4cc810e574a356ef3627f490d104605d3a8d67ce
Sarwar Ahmed <sarwar.amas@gmail.com>
2024-05-18 19:48:04 +0600

    Post request to database with JWT token

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const cookieParser = require('cookie-parser');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
 const app = express();
 
 const corsOptions = {
-    origin: ['http://localhost:5173'],
+    origin: [
+        'http://localhost:5173',
+        'https://himalayansa.firebaseapp.com',
+    ],
     credentials: true,
     optionsSuccessStatus: 200,
 };
@@ -17,23 +20,7 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 
-// verify jwt middleware
-const verifyToken = (req, res, next) => {
-    const token = req.cookies?.token
-    if (!token) return res.status(401).send({ message: 'unauthorized access' })
-    if (token) {
-        jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-            if (err) {
-                console.log(err)
-                return res.status(401).send({ message: 'unauthorized access' })
-            }
-            console.log(decoded)
 
-            req.user = decoded
-            next()
-        })
-    }
-}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5mrfovz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -50,43 +37,50 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-
-        const VolunteerCollection = client.db('soloSphere').collection('post')
-
-        // jwt generate
-        app.post('/jwt', async (req, res) => {
-            const email = req.body
-            const token = jwt.sign(email, process.env.JWT_SECRET, {
-                expiresIn: '365d',
-            })
-            res
-                .cookie('token', token, {
-                    httpOnly: true,
-                    // secure: process.env.NODE_ENV === 'production',
-                    // sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
-                    secure: true,
-                    sameSite: 'None',
-                })
-                .send({ success: true })
-        })
-
-        // Clear token on logout
-        app.get('/logout', (req, res) => {
-            res
-                .clearCookie('token', {
-                    httpOnly: true,
-                    // secure: process.env.NODE_ENV === 'production',
-                    // sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
-                    secure: true,
-                    sameSite: 'None',
-                    maxAge: 0,
-                })
-                .send({ success: true })
-        })
-
-
         // Connect the client to the server	(optional starting in v4.7)
         // await client.connect();
+
+        const postCollection = client.db('himalayan').collection('post');
+
+        // JWT generation
+        app.post('/jwt', (req, res) => {
+            const user = req.body
+            console.log(user);
+            const token = jwt.sign(user, process.env.JWT_SECRET, {
+                expiresIn: '7d'
+            })
+
+            res.cookie('token', token, {
+                httpOnly: true,
+                secure: true,
+                sameSite: 'none',
+                maxAge: 604800000
+            }).send({ success: true })
+
+            // res.cookie('token', token, {
+            //     httpOnly: true,
+            //     secure: process.env.NODE_ENV === 'production',
+            //     sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict'
+            // }).send({ success: true })
+        })
+
+        app.get('/logout', (req, res) => {
+            res.clearCookie('token', {
+                httpOnly: true,
+                secure: true,
+                sameSite: 'none',
+                maxAge: 0
+            }).send({ success: true })
+        })
+
+        app.post('/volunteerPost', async (req, res) => {
+            const post = req.body
+            console.log(post);
+            const result = await postCollection.insertOne(post)
+            res.send(result)
+        })
+
+        const VolunteerCollection = client.db('soloSphere').collection('post')
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
